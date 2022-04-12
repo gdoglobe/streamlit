f239a8cf6970f351425989ab67d66d7f0c358d52
gdoglobe <44995959+gdoglobe@users.noreply.github.com>
2022-04-12 15:45:05 +0200

    Update streamlit_app.py
    cc

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,10 @@
+import streamlit as st
 
+
+@st.cache (allow_output_mutation=True)
+
+st.title('CNN classification imge')
+
+
+    st.write('Precidiction :')
+    st.write('Pourcentage de la prédiction :')
